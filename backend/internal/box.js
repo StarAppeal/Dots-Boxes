397d2a63f156ab1ece64fd9932e36a1fb623ab38
StarAppeal <meisoliver1@gmail.com>
2020-02-25 18:40:34 +0100

    this wasn't commited before

---
+++ backend/internal/box.js
@@ -0,0 +1,12 @@
+class Box {
+  constructor(x, y) {
+      this.top = false;
+      this.right = false;
+      this.bot = false;
+      this.right = false;
+      this.x = x;
+      this.y = y;
+      this.ownedBy = -1; // not owned by anyone
+  }
+
+}
