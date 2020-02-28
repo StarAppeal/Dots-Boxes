87a51d4527856b97b934d17efcdfe4bd67a17aee
tsomic <60610467+tsomic@users.noreply.github.com>
2020-02-28 20:08:58 +0100

    removes unused server var

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const app = require('./app');
 const http = require('http');
 const open = require('open');
-var server;
 var port;
 
-server = http.createServer(app).listen(process.env.PORT, function() {
+http.createServer(app).listen(process.env.PORT, function() {
 	port = this.address().port;
 	console.log("App listens on port: " + port);
 	open('http://localhost:'+port+'/kaesekaestchen');
