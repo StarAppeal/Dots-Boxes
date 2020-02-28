a7690d9cb7633877507c8b0ec24c27197eeca678
tsomic <60610467+tsomic@users.noreply.github.com>
2020-02-28 20:07:10 +0100

    opens browser after server start (npm install open)

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const app = require('./app');
 const http = require('http');
+const open = require('open');
+var server;
+var port;
 
-http.createServer(app).listen(process.env.PORT, function() {
-	console.log("App listens on port: " + this.address().port);
+server = http.createServer(app).listen(process.env.PORT, function() {
+	port = this.address().port;
+	console.log("App listens on port: " + port);
+	open('http://localhost:'+port+'/kaesekaestchen');
 });
-	
