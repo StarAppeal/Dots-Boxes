995eab49d0d8545e6fc700bf7e7e5734ddc0710c
tsomic <60610467+tsomic@users.noreply.github.com>
2020-03-18 20:07:58 +0100

    ts fix?

--- src/public/js/frontend/clickEvents.ts
+++ src/public/js/frontend/clickEvents.ts
@@ -1,6 +1,6 @@
 let retractor = document.getElementById("retractor")
-let users = document.getElementById("users")
 
 retractor.addEventListener("mouseup", function(e) {
-  users.classList.toggle("retracted")
+  let usersEl = document.getElementById("users")
+  usersEl.classList.toggle("retracted")
 })
